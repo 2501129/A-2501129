dc31c53edc11d2fc3fff98181f7910537c6410b5
2501129 <303076898@qq.com>
2025-09-24 15:35:08 +0900

    Add files via upload

---
+++ week03/out/Sampie-4/src/Main.java
@@ -0,0 +1,12 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+    String name = "흥길동";
+    int age = 21;
+    float eye = 1.2f;
+
+
+        System.out.printf("이름 ;%s\n",name);
+        System.out.printf("나이 ;%2d\n",age);
+        System.out.printf("나이 ;%2d\n",eye);
---
+++ week03/scr/src/Main.java
@@ -0,0 +1,15 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+String appie = "부사"; //String apple = new String("부사");
+        byte count = 4;
+        String name = "흥같동";
+        double height = 170.5;
+
+
+
+        System.out.println("사과의 중류는 %이고,개수는 %d개 입니다.\n",apple,count);
+        System.out.println("%님");
+    }
+}
